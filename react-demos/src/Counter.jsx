ca0788c70cb0573db24d037f103c09ea3621da08
meow-codes201 <humairat291@gmail.com>
2026-06-19 06:10:06 +0530

    Counter practice useState

--- react-demos/src/Counter.jsx
+++ react-demos/src/Counter.jsx
@@ -1,52 +1,71 @@
 import { useState } from "react";
 function Counter() {
-  [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   const add = () => {
-    setCount(count++);
+    setCount(count + 1);
   };
   const reset = () => {
     setCount(0);
   };
   const sub = () => {
-    setCount(count--);
+    setCount(count - 1);
   };
 
   return (
     <>
-      <button
-        style={{
-          backgroundColor: "green",
-          width: "50px",
-          cursor: "pointer",
-          color: "white",
-        }}
-        onClick={add}
-      >
-        +
-      </button>
-      <button
-        style={{
-          backgroundColor: "pink",
-          width: "50px",
-          cursor: "pointer",
-          color: "white",
-        }}
-        onClick={reset}
-      >
-        reset
-      </button>
-      <button
-        style={{
-          backgroundColor: "red",
-          width: "50px",
-          cursor: "pointer",
-          color: "white",
-        }}
-        onClick={sub}
-      >
-        -
-      </button>
+      <h1 style={{ fontSize: "6em", margin: "0px", textAlign: "center" }}>
+        {count}
+      </h1>
+      <div style={{ textAlign: "center" }}>
+        <button
+          style={{
+            backgroundColor: "green",
+            width: "100px",
+            height: "100px",
+            border: "1px solid white",
+            margin: "10px",
+            cursor: "pointer",
+            color: "white",
+            fontSize: "2rem",
+          }}
+          onClick={add}
+        >
+          +
+        </button>
+        <button
+          style={{
+            border: "1px solid white",
+
+            fontSize: "2rem",
+            backgroundColor: "pink",
+            width: "100px",
+            height: "100px",
+            margin: "10px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={reset}
+        >
+          reset
+        </button>
+        <button
+          style={{
+            border: "1px solid white",
+            fontSize: "2rem",
+            backgroundColor: "red",
+            width: "100px",
+            height: "100px",
+            margin: "10px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={sub}
+        >
+          -
+        </button>
+      </div>
     </>
   );
 }
+export default Counter;
