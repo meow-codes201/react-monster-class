3bba921cbca22636500b1e8840c25b186668d9d4
meow-codes201 <humairat291@gmail.com>
2026-06-20 13:18:28 +0530

    useReducer Hook

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -15,30 +15,46 @@ import Todo from "./Todo";
 import Profile from "./Profile";
 import ShoppingList from "./ShoppingList";
 import { useState, useEffect } from "react";
-function App() {
-  const [data, setData] = useState([]);
+import BasicEffect from "./BasicEffect";
+import CounterEffect from "./CounterEffect";
+import FetchDataEffect from "./FetchDataEffect";
+import ComponentA from "./ComponentA.jsx";
+import UserContext from "./UserContext.jsx";
+import UpdateUser from "./UpdateUser.jsx";
+import UserProfile from "./UserProfile.jsx";
+import { useReducer } from "react";
 
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-      if (data && data.length) {
-        setData(data);
-      }
-    }
-    getData();
-  }, []);
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "reset":
+      return { ...state, count: 0 };
+    case "incBy5":
+      return { ...state, count: state.count + 5 };
+    case "decBy5":
+      return { ...state, count: state.count - 5 };
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
-      <ul>
-        {data.map((todo) => (
-          <li key={todo.id}>
-            <h3> title - {todo.title}</h3>
-            <p>Body: {todo.body || "No content"}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+      <button onClick={() => dispatch({ type: "incBy5" })}>Add 5</button>
+
+      <button onClick={() => dispatch({ type: "decBy5" })}>Sub 5</button>
     </>
   );
 }
